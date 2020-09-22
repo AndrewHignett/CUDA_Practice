f5b445aedace8d24a92b50c2a1345a28cd08a231
Pinguino25 <andrew.hignett@hotmail.co.uk>
2020-09-22 23:13:13 +0100

    GPU use added to Add Vectors

--- Add Vectors/Add Vectors/vectorAdd.cu
+++ Add Vectors/Add Vectors/vectorAdd.cu
@@ -2,10 +2,11 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-#define N 10000
+#define N 100000000
 
-void vectorAdd(float *out, float *a, float *b, int n)
+__global__ void vectorAdd(float *out, float *a, float *b, int n)
 {
 	for (int i = 0; i < n; i++)
 	{
@@ -17,14 +18,45 @@ void vectorAdd(float *out, float *a, float *b, int n)
 int main()
 {
 	float *a, *b, *out;
+	float *d_a, *d_b, *d_out;
+	//Allocate host memory for a
 	a = (float*)malloc(sizeof(float) * N);
+	//Allocate host memory for b
 	b = (float*)malloc(sizeof(float) * N);
+	//Allocate host memory for out
 	out = (float*)malloc(sizeof(float) * N);
 	for (int i = 0; i < N; i++)
 	{
 		a[i] = 1.0f;
 		b[i] = 2.0f;
 	}
-	vectorAdd(out, a, b, N);
+	//Allocate device memory for a
+	cudaMalloc((void**)&d_a, sizeof(float) * N);
+	//Transfer from host to device memory
+	cudaMemcpy(d_a, a, sizeof(float) * N, cudaMemcpyHostToDevice);
+	//Allocate device memory for b
+	cudaMalloc((void**)&d_b, sizeof(float) * N);
+	//Transfer from host to device memory
+	cudaMemcpy(d_b, b, sizeof(float) * N, cudaMemcpyHostToDevice);
+	//Allocate device memory for out
+	cudaMalloc((void**)&d_out, sizeof(float) * N);
+	//Transfer from host to device memory
+	//cudaMemcpy(d_out, out, sizeof(float) * N, cudaMemcpyHostToDevice);
+	vectorAdd<<<1,1>>>(d_out, d_a, d_b, N);
+	cudaMemcpy(out, d_out, sizeof(float) * N, cudaMemcpyDeviceToHost);
+	cudaFree(d_a);
+	cudaFree(d_b);
+	cudaFree(d_out);
+	free(a);
+	free(b);
+	free(out);
     return 0;
 }
+
+/*
+1. Allocate host memory and initialized host data
+2. Allocate device memory
+3. Transfer input data from host to device memory
+4. Execute kernels
+5. Transfer output from device memory to host
+*/
