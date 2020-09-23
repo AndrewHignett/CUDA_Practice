7b0051ab26788716332b7adf3817de824affc58d
Pinguino25 <andrew.hignett@hotmail.co.uk>
2020-09-23 23:30:46 +0100

    Dot Product fixed cudaMemcpy

--- Dot Product/DotProduct/dotProduct.cu
+++ Dot Product/DotProduct/dotProduct.cu
@@ -8,28 +8,17 @@
 #ifndef __CUDACC__  
 #define __CUDACC__
 #endif
-#define N 1//1024
+#define N 1024
 
 __global__ void dotProduct(float *out, float *a, float *b)
 {
-	//int index = threadIdx.x + blockDim.x*blockIdx.x;
-	//int step = blockDim.x*gridDim.x;
 	//Shared memory for multiplication
 	__shared__ float cache[N];
-	//float temp = 0.0;
-	//while (index < N)
-	//{
-	//	temp += a[index] * b[index];
-	//	index += step;
-	//}
-	//cache[threadIdx.x] = temp;
 	cache[threadIdx.x] = a[threadIdx.x] * b[threadIdx.x];
 
-	//printf("%f %f\n", a[threadIdx.x], b[threadIdx.x]);
 	//synchronise threads
 	__syncthreads();
 
-	//int i = blockDim.x / 2;
 	//Use thread 0 to sum the products
 	if (threadIdx.x == 0)
 	{		
@@ -37,23 +26,22 @@ __global__ void dotProduct(float *out, float *a, float *b)
 		for (int i = 0; i < N; i++)
 		{
 			sum += cache[i];
-		}	
-		printf("%f\n", sum);
+		}
 		*out = sum;
 	}
 }
 
 int main()
 {
-	float *a, *b, *out;
+	float *a, *b, out;
 	float *d_a, *d_b, *d_out;
 	//Allocate host memory for a
 	a = (float*)malloc(sizeof(float) * N);
 	//Allocate host memory for b
 	b = (float*)malloc(sizeof(float) * N);
 	//Allocate host memory for out
-	out = (float*)malloc(sizeof(float));
-	out[0] = 1.0f;
+	//out = (float*)malloc(sizeof(float));
+	out = 1.0f;
 	for (int i = 0; i < N; i++)
 	{
 		a[i] = 3.0f;
@@ -70,19 +58,15 @@ int main()
 	//Allocate device memeory for out
 	cudaMalloc((void**)&d_out, sizeof(float));
 	//Transfer from host memory to device memory
-	cudaMemcpy(d_out, out, sizeof(float), cudaMemcpyHostToDevice);
-	//Run function in parallel in 1024 threads
-	printf("%f\n", a[0]);
-	printf("%f\n", b[0]);
-	printf("%f\n", out[0]);
+	cudaMemcpy(d_out, &out, sizeof(float), cudaMemcpyHostToDevice);
+	//Run function in parallel in 1024 threads, on a single grid
 	dotProduct<<<1, N>>>(d_out, d_a, d_b);
-	cudaMemcpy(out, d_out, sizeof(float), cudaMemcpyDeviceToHost);
-	printf("Device %f\n", d_out[0]);
+	cudaMemcpy(&out, d_out, sizeof(float), cudaMemcpyDeviceToHost);
 	cudaFree(d_a);
 	cudaFree(d_b);
 	cudaFree(d_out);
 	free(a);
 	free(b);
-	free(out);
+	//free(out);
 	return 0;
 }
